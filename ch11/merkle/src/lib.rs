a7496858ae032720eb928ead63af2fb150d7deb0
Keith Noguchi <hack@noguchi.us>
2022-12-20 17:19:09 -0800

    merkle: proof returns MerkleProofIter
    Now the proof function returns
    the MerkleProofIter iterator to
    give the consumer a flexibility
    of the proof operation.

--- ch11/merkle/src/lib.rs
+++ ch11/merkle/src/lib.rs
@@ -3,6 +3,7 @@ use digest::{Digest, Output, OutputSizeUser};
 use generic_array::ArrayLength;
 use std::error::Error;
 use std::fmt::Debug;
+use std::io;
 use std::mem;
 use std::ops::Range;
 use std::result;
@@ -39,7 +40,7 @@ where
 
         // set the leaf node first with the provided hash.
         let mut node = NodeData::try_from(hash)?;
-        tree.leaves_mut().for_each(|leaf| *leaf = node.clone());
+        tree.leaves_iter_mut().for_each(|leaf| *leaf = node.clone());
 
         // then calculate the merkle root.
         for depth in (1..depth).rev() {
@@ -68,17 +69,12 @@ where
 
     /// Panic when called on the empty tree.
     pub fn leaves(&self) -> impl Iterator<Item = &[u8]> {
-        self.data[self.leaf_start..]
-            .iter()
-            .map(|node| node.as_ref())
+        self.leaves_iter().map(|node| node.as_ref())
     }
 
     #[instrument(name = "MerkleTree::set", skip(self), err)]
-    pub fn set(&mut self, index: usize, hash: &[u8]) -> Result<()> {
-        let node = match self.leaves_mut().nth(index) {
-            None => Err("invalid leaf offset")?,
-            Some(node) => node,
-        };
+    pub fn set(&mut self, leaf_offset: usize, hash: &[u8]) -> Result<()> {
+        let node = self.try_leaf_mut(leaf_offset)?;
         if node.as_ref() == hash {
             // no change.
             return Ok(());
@@ -86,27 +82,14 @@ where
         *node = NodeData::try_from(hash)?;
 
         // calculate the merkle root.
-        for _ in self.merkle_root_iter(self.leaf_start + index) {}
+        for _ in self.parent_hash_iter_mut(self.leaf_start + leaf_offset) {}
 
         Ok(())
     }
 
-    pub fn proof(&self, leaf_offset: usize) -> Result<Vec<(Position, Output<B>)>> {
-        let leaf_index = self.leaf_index(leaf_offset)?;
-        let mut proof = vec![];
-        match self.proof_pair(leaf_index) {
-            None => Err("missing hash for the leaf pair")?,
-            Some(pair) => {
-                proof.push(pair);
-            }
-        }
-        for ancester in ancesters(leaf_index) {
-            match self.proof_pair(ancester) {
-                Some(pair) => proof.push(pair),
-                None => break,
-            }
-        }
-        Ok(proof)
+    pub fn proof(&self, leaf_offset: usize) -> Result<MerkleProofIter<B>> {
+        let _node = self.try_leaf(leaf_offset)?;
+        Ok(self.merkle_proof_iter(self.leaf_start + leaf_offset))
     }
 
     // Make this associated function private, as it doesn't completely
@@ -131,36 +114,47 @@ where
         }
     }
 
-    fn leaves_mut(&mut self) -> impl Iterator<Item = &mut NodeData<B>> {
+    fn try_leaf(&self, index: usize) -> Result<&NodeData<B>> {
+        self.leaves_iter().nth(index).ok_or_else(|| {
+            io::Error::new(
+                io::ErrorKind::InvalidInput,
+                format!("invalid leaf index: {index}"),
+            )
+            .into()
+        })
+    }
+
+    fn try_leaf_mut(&mut self, index: usize) -> Result<&mut NodeData<B>> {
+        self.leaves_iter_mut().nth(index).ok_or_else(|| {
+            io::Error::new(
+                io::ErrorKind::InvalidInput,
+                format!("invalid leaf index: {index}"),
+            )
+            .into()
+        })
+    }
+
+    fn leaves_iter(&self) -> impl Iterator<Item = &NodeData<B>> {
+        self.data[self.leaf_start..].iter()
+    }
+
+    fn leaves_iter_mut(&mut self) -> impl Iterator<Item = &mut NodeData<B>> {
         self.data[self.leaf_start..].iter_mut()
     }
 
-    fn merkle_root_iter(&mut self, index: usize) -> MerkleRootIter<B> {
+    fn parent_hash_iter_mut(&mut self, index: usize) -> ParentHashIterMut<B> {
         let index = if index & 1 == 1 { index + 1 } else { index };
         assert!(index < self.data.len(), "invalid child index");
-        MerkleRootIter {
+        ParentHashIterMut {
             data: &mut self.data[..=index],
         }
     }
 
-    fn proof_pair(&self, index: usize) -> Option<(Position, Output<B>)> {
-        sibling(index)
-            .map(|sibling| (&self.data[sibling]).into())
-            .map(|hash| (Position::from(index), hash))
-    }
-
-    fn leaf_index(&self, leaf_offset: usize) -> Result<usize> {
-        let leaf_range = self.leaf_range();
-        let leaf_index = leaf_range.start + leaf_offset;
-        if !leaf_range.contains(&leaf_index) {
-            Err("invalid leaf offset")?;
+    fn merkle_proof_iter(&self, index: usize) -> MerkleProofIter<B> {
+        MerkleProofIter {
+            index,
+            data: &self.data,
         }
-        Ok(leaf_index)
-    }
-
-    #[inline]
-    fn leaf_range(&self) -> Range<usize> {
-        self.depth_range(self.tree_depth).unwrap()
     }
 
     fn try_nodes_in_depth_mut(
@@ -190,11 +184,91 @@ where
     }
 }
 
+/// MerkleProofIter for the merkle proof.
+#[derive(Debug)]
+pub struct MerkleProofIter<'a, B>
+where
+    B: Debug + OutputSizeUser,
+    Data<B>: Copy,
+{
+    index: usize,
+    data: &'a [NodeData<B>],
+}
+
+impl<'a, B> Iterator for MerkleProofIter<'a, B>
+where
+    B: Debug + OutputSizeUser,
+    Data<B>: Copy,
+{
+    type Item = MerkleNode<'a, B>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index == 0 {
+            return None;
+        }
+        let (kind, start, end) = if self.index & 1 == 1 {
+            (MerkleNodeKind::Left, self.index, self.index + 1)
+        } else {
+            (MerkleNodeKind::Right, self.index - 1, self.index)
+        };
+        self.index = (self.index - 1) / 2;
+        Some(MerkleNode {
+            kind,
+            data: &self.data[start..=end],
+        })
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum MerkleNodeKind {
+    Root,
+    Left,
+    Right,
+}
+
+#[derive(Debug)]
+pub struct MerkleNode<'a, B>
+where
+    B: Debug + OutputSizeUser,
+    Data<B>: Copy,
+{
+    kind: MerkleNodeKind,
+    data: &'a [NodeData<B>],
+}
+
+impl<'a, B> MerkleNode<'a, B>
+where
+    B: Debug + OutputSizeUser,
+    Data<B>: Copy,
+{
+    pub fn kind(&self) -> MerkleNodeKind {
+        self.kind
+    }
+
+    pub fn data(&self) -> &[u8] {
+        match self.kind {
+            MerkleNodeKind::Root | MerkleNodeKind::Left => self.data[0].as_ref(),
+            MerkleNodeKind::Right => self.data[1].as_ref(),
+        }
+    }
+
+    pub fn sibling_data(&self) -> Option<&[u8]> {
+        match self.kind {
+            MerkleNodeKind::Root => None,
+            MerkleNodeKind::Left => Some(self.data[1].as_ref()),
+            MerkleNodeKind::Right => Some(self.data[0].as_ref()),
+        }
+    }
+}
+
 /// Merkle tree node data.
 ///
 /// It provides the convenient way to access the actual hash value
 /// through the deref method.  The node is always initialized,
 /// e.g., Some(Output<B>) by MerkleTree type.
+///
+/// It's a private type to provide AsRef<[u8]> to the actual
+/// data.
 #[derive(Copy, Debug)]
 struct NodeData<B>(Option<Output<B>>)
 where
@@ -268,7 +342,7 @@ where
     }
 }
 
-struct MerkleRootIter<'a, B>
+struct ParentHashIterMut<'a, B>
 where
     B: Debug + Digest + OutputSizeUser,
     Data<B>: Copy,
@@ -276,7 +350,7 @@ where
     data: &'a mut [NodeData<B>],
 }
 
-impl<'a, B> Iterator for MerkleRootIter<'a, B>
+impl<'a, B> Iterator for ParentHashIterMut<'a, B>
 where
     B: Debug + Digest + OutputSizeUser,
     Data<B>: Copy,
@@ -311,25 +385,6 @@ where
     }
 }
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
-pub enum Position {
-    Root,
-    Left,
-    Right,
-}
-
-impl From<usize> for Position {
-    fn from(index: usize) -> Self {
-        if index == 0 {
-            Self::Root
-        } else if index & 0x1 == 0x1 {
-            Self::Left
-        } else {
-            Self::Right
-        }
-    }
-}
-
 pub fn index(depth: usize, offset: usize) -> usize {
     let width = 0x1 << depth;
     assert!(offset < width, "invalid offset");
@@ -349,29 +404,12 @@ fn depth_and_offset(index: usize) -> (usize, usize) {
     (depth, offset)
 }
 
-fn parent(index: usize) -> Option<usize> {
+pub fn parent(index: usize) -> Option<usize> {
     if index == 0 {
         None
     } else {
         Some((index - 1) >> 1)
     }
-}
-
-fn sibling(index: usize) -> Option<usize> {
-    match Position::from(index) {
-        Position::Root => None,
-        Position::Left => Some(index + 1),
-        Position::Right => Some(index - 1),
-    }
-}
-
-fn ancesters(mut index: usize) -> Vec<usize> {
-    let mut ancesters = vec![];
-    while let Some(parent) = parent(index) {
-        ancesters.push(parent);
-        index = parent;
-    }
-    ancesters
 }
 
 pub fn base(index: usize) -> usize {
@@ -381,34 +419,33 @@ pub fn base(index: usize) -> usize {
 
 #[cfg(test)]
 mod tests {
-    use super::{ancesters, parent, sibling};
-    use super::{base, depth_and_offset, index};
-    use super::{MerkleTree, Position};
+    use super::{base, depth_and_offset, index, parent};
+    use super::{MerkleNodeKind, MerkleTree};
     use hex_literal::hex;
     use sha3::Sha3_256;
-    use std::ops::Range;
 
     #[test]
     fn tree_proof() {
         let tree = TreeBuilder::build(5);
-        let got = tree.proof(3).unwrap();
+
+        let got: Vec<_> = tree.proof(3).unwrap().collect();
         assert_eq!(got.len(), 4);
-        assert_eq!(got[0].0, Position::Right);
-        assert_eq!(got[0].1, [0x22; 32].into());
-        assert_eq!(got[1].0, Position::Right);
+        assert_eq!(got[0].kind(), MerkleNodeKind::Right);
+        assert_eq!(got[0].sibling_data().unwrap(), &[0x22; 32]);
+        assert_eq!(got[1].kind(), MerkleNodeKind::Right);
         assert_eq!(
-            got[1].1,
-            hex!("35e794f1b42c224a8e390ce37e141a8d74aa53e151c1d1b9a03f88c65adb9e10").into(),
+            got[1].sibling_data().unwrap(),
+            hex!("35e794f1b42c224a8e390ce37e141a8d74aa53e151c1d1b9a03f88c65adb9e10"),
         );
-        assert_eq!(got[2].0, Position::Left);
+        assert_eq!(got[2].kind(), MerkleNodeKind::Left);
         assert_eq!(
-            got[2].1,
-            hex!("26fca7737f48fa702664c8b468e34c858e62f51762386bd0bddaa7050e0dd7c0").into(),
+            got[2].sibling_data().unwrap(),
+            hex!("26fca7737f48fa702664c8b468e34c858e62f51762386bd0bddaa7050e0dd7c0"),
         );
-        assert_eq!(got[3].0, Position::Left);
+        assert_eq!(got[3].kind(), MerkleNodeKind::Left);
         assert_eq!(
-            got[3].1,
-            hex!("e7e11a86a0c1d8d8624b1629cb58e39bb4d0364cb8cb33c4029662ab30336858").into(),
+            got[3].sibling_data().unwrap(),
+            hex!("e7e11a86a0c1d8d8624b1629cb58e39bb4d0364cb8cb33c4029662ab30336858"),
         );
     }
 
@@ -464,22 +501,6 @@ mod tests {
             2,
         );
         assert!(TreeBuilder::build(2).try_nodes_in_depth_mut(3).is_err());
-    }
-
-    #[test]
-    fn tree_leaf_range() {
-        assert_eq!(
-            TreeBuilder::build(1).leaf_range(),
-            Range { start: 0, end: 1 },
-        );
-        assert_eq!(
-            TreeBuilder::build(2).leaf_range(),
-            Range { start: 1, end: 3 },
-        );
-        assert_eq!(
-            TreeBuilder::build(5).leaf_range(),
-            Range { start: 15, end: 31 },
-        );
     }
 
     #[test]
@@ -576,52 +597,6 @@ mod tests {
         assert_eq!(parent(12), Some(5));
         assert_eq!(parent(13), Some(6));
         assert_eq!(parent(14), Some(6));
-    }
-
-    #[test]
-    fn test_siblig() {
-        assert_eq!(sibling(0), None);
-        assert_eq!(sibling(1), Some(2));
-        assert_eq!(sibling(2), Some(1));
-        assert_eq!(sibling(3), Some(4));
-        assert_eq!(sibling(4), Some(3));
-        assert_eq!(sibling(5), Some(6));
-        assert_eq!(sibling(6), Some(5));
-    }
-
-    #[test]
-    fn test_ancesters() {
-        assert_eq!(ancesters(0), vec![]);
-        assert_eq!(ancesters(1), vec![0]);
-        assert_eq!(ancesters(2), vec![0]);
-        assert_eq!(ancesters(3), vec![1, 0]);
-        assert_eq!(ancesters(4), vec![1, 0]);
-        assert_eq!(ancesters(5), vec![2, 0]);
-        assert_eq!(ancesters(6), vec![2, 0]);
-        assert_eq!(ancesters(7), vec![3, 1, 0]);
-        assert_eq!(ancesters(8), vec![3, 1, 0]);
-        assert_eq!(ancesters(9), vec![4, 1, 0]);
-        assert_eq!(ancesters(10), vec![4, 1, 0]);
-        assert_eq!(ancesters(11), vec![5, 2, 0]);
-        assert_eq!(ancesters(12), vec![5, 2, 0]);
-        assert_eq!(ancesters(13), vec![6, 2, 0]);
-        assert_eq!(ancesters(14), vec![6, 2, 0]);
-        assert_eq!(ancesters(15), vec![7, 3, 1, 0]);
-        assert_eq!(ancesters(16), vec![7, 3, 1, 0]);
-        assert_eq!(ancesters(17), vec![8, 3, 1, 0]);
-        assert_eq!(ancesters(18), vec![8, 3, 1, 0]);
-        assert_eq!(ancesters(19), vec![9, 4, 1, 0]);
-        assert_eq!(ancesters(20), vec![9, 4, 1, 0]);
-        assert_eq!(ancesters(21), vec![10, 4, 1, 0]);
-        assert_eq!(ancesters(22), vec![10, 4, 1, 0]);
-        assert_eq!(ancesters(23), vec![11, 5, 2, 0]);
-        assert_eq!(ancesters(24), vec![11, 5, 2, 0]);
-        assert_eq!(ancesters(25), vec![12, 5, 2, 0]);
-        assert_eq!(ancesters(26), vec![12, 5, 2, 0]);
-        assert_eq!(ancesters(27), vec![13, 6, 2, 0]);
-        assert_eq!(ancesters(28), vec![13, 6, 2, 0]);
-        assert_eq!(ancesters(29), vec![14, 6, 2, 0]);
-        assert_eq!(ancesters(30), vec![14, 6, 2, 0]);
     }
 
     #[test]
