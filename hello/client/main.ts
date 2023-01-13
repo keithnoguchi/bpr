3822a33090cec20f38ec29773ed3ba8f2c9dc2a7
Keith Noguchi <keith@noguchi.us>
2023-01-13 14:09:41 -0800

    hello/client: Calculate the minimum rent exemption fee

--- hello/client/main.ts
+++ hello/client/main.ts
@@ -28,8 +28,30 @@ async function main() {
 
   const conn = await establishConnection("http://127.0.0.1:8899");
   console.log("connection to cluster established", conn);
+
+  await establishPayer(conn);
 }
 
 async function establishConnection(url: string): Promise<Connection> {
   return new Connection(url, "confirmed");
+}
+
+async function establishPayer(conn: Connection) {
+  class GreetingAccount {
+    counter = 0;
+    constructor(fields: {counter: number} | undefined = undefined) {
+      if (fields) {
+        this.counter = fields.counter;
+      }
+    }
+    static GreetingSchema = new Map([
+      [GreetingAccount, {kind: 'struct', fields: [['counter', 'u8']]}],
+    ]);
+    static SIZE = borsh.serialize(
+      GreetingAccount.GreetingSchema,
+      new GreetingAccount(),
+    ).length;
+  }
+  let fees = await conn.getMinimumBalanceForRentExemption(GreetingAccount.SIZE);
+  console.log("minimum fee for the rent exemption", fees);
 }
