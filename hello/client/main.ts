4f8858621a75788e1bb9199c3f1786e10bbe131c
Keith Noguchi <keith@noguchi.us>
2023-01-13 15:05:21 -0800

    hello/client: payer parsing

--- hello/client/main.ts
+++ hello/client/main.ts
@@ -14,6 +14,10 @@ import {
   sendAndConfirmTransaction,
 } from "@solana/web3.js";
 import * as borsh from "borsh";
+import os from "os";
+import fs from "mz/fs";
+import path from "path";
+import yaml from "yaml";
 
 main().then(
   () => process.exit(),
@@ -29,14 +33,15 @@ async function main() {
   const conn = await establishConnection("http://127.0.0.1:8899");
   console.log("connection to cluster established", conn);
 
-  await establishPayer(conn);
+  const payer = await establishPayer(conn);
+  console.log("payer of this transaction, and rent", payer);
 }
 
 async function establishConnection(url: string): Promise<Connection> {
   return new Connection(url, "confirmed");
 }
 
-async function establishPayer(conn: Connection) {
+async function establishPayer(conn: Connection): Promise<Keypair> {
   class GreetingAccount {
     counter = 0;
     constructor(fields: {counter: number} | undefined = undefined) {
@@ -52,6 +57,45 @@ async function establishPayer(conn: Connection) {
       new GreetingAccount(),
     ).length;
   }
-  let fees = await conn.getMinimumBalanceForRentExemption(GreetingAccount.SIZE);
-  console.log("minimum fee for the rent exemption", fees);
+  const size = GreetingAccount.SIZE;
+  let fees = await conn.getMinimumBalanceForRentExemption(size);
+  console.log("minimum fee for the rent exemption", fees, "for", size, "Byte(s)");
+  const {feeCalculator} = await conn.getRecentBlockhash();
+  const fee_for_one_signature = feeCalculator.lamportsPerSignature;
+  console.log("transaction fee for a single signature", fee_for_one_signature);
+  fees += fee_for_one_signature; // just one signature.
+  console.log("total transaction fee", fees);
+
+  return await getPayer();
+}
+
+async function getPayer(): Promise<Keypair> {
+  try {
+    const config = await getConfig();
+    if (!config.keypair_path) throw new Error("Missing keypair path");
+    return await createKeypairFromFile(config.keypair_path);
+  } catch (e) {
+    console.warn(
+      "Failed to create keypair from CLI config file, falling back to new random keypair",
+    );
+    return Keypair.generate();
+  }
+}
+
+async function getConfig(): Promise<any> {
+  const CONFIG_FILE_PATH = path.resolve(
+    os.homedir(),
+    ".config",
+    "solana",
+    "cli",
+    "config.yml",
+  );
+  const configYaml = await fs.readFile(CONFIG_FILE_PATH, {encoding: "utf-8"});
+  return yaml.parse(configYaml);
+}
+
+async function createKeypairFromFile(filePath: string): Promise<Keypair> {
+  const secretKeyString = await fs.readFile(filePath, {encoding: "utf-8"});
+  const secretKey = Uint8Array.from(JSON.parse(secretKeyString));
+  return Keypair.fromSecretKey(secretKey);
 }
