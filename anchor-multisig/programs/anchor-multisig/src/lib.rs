635f0128f9661a625dffd3eb608ed0c371e13240
Keith Noguchi <keith@noguchi.us>
2023-01-20 14:36:47 -0800

    anchor-multisig: Create a multisig account
    This is just a skeleton but it DOES create
    the multisig account with 200 bytes?!
    
    It's a great example to allow caller of the
    program to specify the size of the account,
    instead of hard coding the data account size
    in the program itself.

--- anchor-multisig/programs/anchor-multisig/src/lib.rs
+++ anchor-multisig/programs/anchor-multisig/src/lib.rs
@@ -1,3 +1,16 @@
+//! An anchor Multisig program.
+//!
+//! It's demonstrated in [coral-xyz] as well as the
+//! [anchor tests].
+//!
+//! Those two files are almost identical, except that
+//! the exported one, not the one under [anchor test],
+//! has an added functionality, e.g. unique owner check.
+//!
+//! This example follows, e.g. steals, ;), the exported one.
+//!
+//! [coral-xyz]: https://github.com/coral-xyz/multisig/blob/master/programs/multisig/src/lib.rs
+//! [anchor tests]: https://github.com/coral-xyz/anchor/blob/master/tests/multisig/programs/multisig/src/lib.rs
 use anchor_lang::prelude::*;
 
 declare_id!("EYg7btAzuDC6MoYeCN9YzZcWu3T25Xqt7SEhcTbdbnG2");
@@ -6,10 +19,26 @@ declare_id!("EYg7btAzuDC6MoYeCN9YzZcWu3T25Xqt7SEhcTbdbnG2");
 pub mod anchor_multisig {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_multisig(
+        ctx: Context<CreateMultisig>,
+        owners: Vec<Pubkey>,
+        threshold: u64,
+        nonce: u8,
+    ) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateMultisig<'info> {
+    #[account(zero, signer)]
+    multisig: Box<Account<'info, Multisig>>,
+}
+
+#[account]
+pub struct Multisig {
+    pub owners: Vec<Pubkey>,
+    pub threshold: u64,
+    pub nonce: u8,
+    pub owner_set_seqno: u32,
+}
--- anchor-multisig/tests/anchor-multisig.ts
+++ anchor-multisig/tests/anchor-multisig.ts
@@ -8,9 +8,51 @@ describe("anchor-multisig", () => {
 
   const program = anchor.workspace.AnchorMultisig as Program<AnchorMultisig>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("creates multisig account", async () => {
+    const multisig = anchor.web3.Keypair.generate();
+
+    // Get the nonce for the PDA based on the multisig
+    // address.  The nonce is stored in the multisig
+    // data account and the multisigSigner will be used
+    // for the transaction creation below.
+    const [multisigSigner, nonce] =
+      await anchor.web3.PublicKey.findProgramAddress(
+        [multisig.publicKey.toBuffer()],
+        program.programId
+      );
+
+    // We don't need this big size for the multisig
+    // account.  I'll come back to this size later.
+    const multisigSize = 200;
+
+    // 3/5 multisig example.
+    const ownerA = anchor.web3.Keypair.generate();
+    const ownerB = anchor.web3.Keypair.generate();
+    const ownerC = anchor.web3.Keypair.generate();
+    const ownerD = anchor.web3.Keypair.generate();
+    const ownerE = anchor.web3.Keypair.generate();
+    const owners = [
+      ownerA.publicKey,
+      ownerB.publicKey,
+      ownerC.publicKey,
+      ownerD.publicKey,
+      ownerE.publicKey,
+    ];
+    const threshold = new anchor.BN(3);
+
+    const tx = await program.rpc.createMultisig(owners, threshold, nonce, {
+      accounts: {
+        multisig: multisig.publicKey,
+      },
+      instructions: [
+        await program.account.multisig.createInstruction(
+          multisig,
+          multisigSize
+        ),
+      ],
+      signers: [multisig],
+    });
+
+    console.log("Multisig account had been created", tx);
   });
 });
