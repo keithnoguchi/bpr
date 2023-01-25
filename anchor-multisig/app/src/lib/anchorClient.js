18390deaea4b02928c8625369cb102342060e26b
Keith Noguchi <keith@noguchi.us>
2023-01-24 18:57:11 -0800

    anchor-multisig: Get the wallet address

--- anchor-multisig/app/src/lib/anchorClient.js
+++ anchor-multisig/app/src/lib/anchorClient.js
@@ -1,2 +1,24 @@
-import idl from "../../../target/idl/anchor_multisig.json"
+import * as anchor from "@project-serum/anchor";
+import idl from "../../../target/idl/anchor_multisig.json";
 import programKeypair from "../../../target/deploy/anchor_multisig-keypair.json"
+
+import { SystemProgram } = anchor.web3;
+
+export default class AnchorClient {
+  constructor({ programId, config, keypair } = {}) {
+    this.programId = programId;
+    this.config = config;
+    this.connection = new anchor.web3.Connection(this.config.httpUri, 'confirm');
+    console.log('\n\nConnected to', this.config.httpUri);
+
+    const wallet =
+      window.solana.isConnected && window.solana?.isPhantom
+        ? new WalletAdaptorPhantom()
+        : keypair
+        ? new anchor.Wallet(keypair)
+        : new anchor.Wallet(anchor.web3.Keypair.generate());
+
+    this.provider = new anchor.Provider(this.connection, wallet, opts);
+    this.program = new anchor.Program(idl, this.programId, this.provider);
+  }
+}
--- anchor-multisig/app/src/routes/+page.svelte
+++ anchor-multisig/app/src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import * as idl from "../../../target/idl/anchor_multisig.json";
+  import * as AnchorMultisig from "../../../target/types/anchor_multisig";
+  import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+
+  import { onMount } from 'svelte';
+
+  let wallet;
+  let account = "";
+  $: account && console.log(`Connected to wallet: ${account}`);
+
+  const handleConnectWallet = async () => {
+    const resp = await wallet.connect();
+  };
+
+  onMount(async () => {
+    console.log("mounted");
+    const { solana } = window;
+    wallet = solana;
+
+    wallet.on("connect", () => (account = wallet.publicKey.toString()));
+    wallet.on("disconnect", () => (account = ""));
+
+    const resp = await wallet.connect({ onlyIfTrusted: true });
+  });
+</script>
+
+<h1>anchor-multisig</h1>
+{#if account}
+<h3>You wallet ID</h3>
+<p>{account}</p>
+{:else if wallet}
+  {#if wallet.isPhantom}
+    <h2>Phantom Wallet found!</h2>
+    <button on:click="{handleConnectWallet}">Connect wallet</button>
+  {:else}
+    <h2>Solana wallet found but not supported.</h2>
+  {/if}
+{:else}
+  <h2>Solana wallet not found.</h2>
+{/if}
