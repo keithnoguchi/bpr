81304f55372abaf885ac38b508042870435b1338
Keith Noguchi <keith@noguchi.us>
2023-01-24 22:10:49 -0800

    anchor-multisig: Cleaning up the pages

---
+++ anchor-multisig/app/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import * as idl from "../../../target/idl/anchor_multisig.json";
+  import * as AnchorMultisig from "../../../target/types/anchor_multisig";
+  import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+</script>
+
+<slot />
--- anchor-multisig/app/src/routes/+page.svelte
+++ anchor-multisig/app/src/routes/+page.svelte
@@ -1,8 +1,4 @@
-<script lang="ts">
-  import * as idl from "../../../target/idl/anchor_multisig.json";
-  import * as AnchorMultisig from "../../../target/types/anchor_multisig";
-  import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
-
+<script>
   import { onMount } from 'svelte';
 
   let wallet;
