eaffdcc12b11325b018fd30f3bd43f30a84b00b4
Keith Noguchi <keith@noguchi.us>
2023-01-25 20:50:21 -0800

    anchor-counter/app: Shows the wallet button
    And it also detect/connect to phantom wallet.

--- anchor-counter/app/src/routes/+layout.svelte
+++ anchor-counter/app/src/routes/+layout.svelte
@@ -2,11 +2,27 @@
 	import { onMount } from 'svelte';
 	import { WalletProvider } from '@svelte-on-solana/wallet-adapter-ui';
 	import { AnchorConnectionProvider } from '@svelte-on-solana/wallet-adapter-anchor';
+	import idl from '../../../target/idl/anchor_counter.json';
 
+	const localStorageKey = 'walletAdapter';
 	const network = 'http://127.0.0.1:8899';
+	let wallets;
 
 	onMount(async () => {
-		console.log(`${network}`);
+		const {
+			PhantomWalletAdapter
+		} = await import('@solana/wallet-adapter-wallets');
+
+		const walletsMap = [
+			new PhantomWalletAdapter(),
+		];
+		wallets = walletsMap
 	});
 </script>
-<slot />
+
+<WalletProvider { localStorageKey } { wallets } autoConnect />
+<AnchorConnectionProvider {network} {idl} />
+
+<div>
+	<slot />
+</div>
--- anchor-counter/app/src/routes/+page.svelte
+++ anchor-counter/app/src/routes/+page.svelte
@@ -1,1 +1,9 @@
+<script lang="ts">
+	import { WalletMultiButton } from '@svelte-on-solana/wallet-adapter-ui';
+</script>
+
 <h1>anchor-counter</h1>
+
+<div class="address">
+	<WalletMultiButton />
+</div>
