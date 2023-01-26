ed6b53e2be64f29884176ed0a964be6d582f7be4
Keith Noguchi <keith@noguchi.us>
2023-01-25 21:45:14 -0800

    anchor-counter/app: Counter initialization completes

--- anchor-counter/app/src/routes/+page.svelte
+++ anchor-counter/app/src/routes/+page.svelte
@@ -1,9 +1,41 @@
 <script lang="ts">
 	import { walletStore } from '@svelte-on-solana/wallet-adapter-core';
 	import { workSpace } from '@svelte-on-solana/wallet-adapter-anchor';
+
+	let counter;
+
+	$: console.log('counter: ', counter);
+
+	async function initialize() {
+		try {
+			const tx = await $workSpace
+				.program
+				.methods
+				.initialize()
+				.accounts({
+					state: $workSpace.baseAccount.publicKey,
+					authority: $walletStore.publicKey,
+				})
+				.signers([$workSpace.baseAccount])
+				.rpc();
+			console.log("Init done", tx);
+
+			const account = await $workSpace
+				.program
+				.account
+				.state
+				.fetch($workSpace.baseAccount.publicKey);
+			counter = account.count.toString();
+		} catch (e) {
+			console.log('Error: ', e);
+		}
+	}
 </script>
 
 {#if $walletStore?.connected}
-	<p>Your wallet address is {$walletStore.publicKey}</p>
-	<p>Counter's program ID is {$workSpace.program.programId}</p>
+	{#if counter}
+		Counter is {counter}
+	{:else}
+		<button on:click={initialize}>Initialize counter</button>
+	{/if}
 {/if}
