c2cd7e0251bf867605b2c79ec01a02aa932dca65
Keith Noguchi <keith@noguchi.us>
2023-01-25 20:53:06 -0800

    anchor-counter: Showing the wallet connection status
    with $walletStore local store variable.

--- anchor-counter/app/src/routes/+page.svelte
+++ anchor-counter/app/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { WalletMultiButton } from '@svelte-on-solana/wallet-adapter-ui';
+	import { walletStore } from '@svelte-on-solana/wallet-adapter-core';
 </script>
 
 <h1>anchor-counter</h1>
@@ -7,3 +8,9 @@
 <div class="address">
 	<WalletMultiButton />
 </div>
+
+{#if $walletStore?.connected}
+	<p>wallet connected</p>
+{:else}
+	<p>wallet not connected</p>
+{/if}
