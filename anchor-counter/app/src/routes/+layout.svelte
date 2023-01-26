b79ae3e76ec50b9eaf7282af97d57eef046ad2cf
Keith Noguchi <keith@noguchi.us>
2023-01-25 20:42:18 -0800

    anchor-counter/app: Make importing anchor success
    vite.config.js process environment setting is the
    trick.

--- anchor-counter/app/src/routes/+layout.svelte
+++ anchor-counter/app/src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
 	import { onMount } from 'svelte';
+	import { WalletProvider } from '@svelte-on-solana/wallet-adapter-ui';
+	import { AnchorConnectionProvider } from '@svelte-on-solana/wallet-adapter-anchor';
 
 	const network = 'http://127.0.0.1:8899';
 
--- anchor-counter/app/vite.config.js
+++ anchor-counter/app/vite.config.js
@@ -1,7 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	define: {
+		// This makes @project-serum/anchor's process error
+		// not happen since it replaces all instances of
+		// process.env.BROWSER with true.
+		'process.env.BROWSER': true,
+		'process.env.NODE_DEBUG': JSON.stringify('')
+	}
 };
 
 export default config;
