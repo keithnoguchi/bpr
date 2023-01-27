df39c27d4b049adb7cf19cbfd143ae994088777f
Keith Noguchi <keith@noguchi.us>
2023-01-26 23:45:56 -0800

    anchor-pda-user-stats: Open and Close PDAs

--- anchor-pda-user-stats/programs/anchor-pda-user-stats/src/lib.rs
+++ anchor-pda-user-stats/programs/anchor-pda-user-stats/src/lib.rs
@@ -2,14 +2,90 @@ use anchor_lang::prelude::*;
 
 declare_id!("3EQwhZWFzX1MCUbYpckbErrrLmt5n9PEQdjHUpXc8as7");
 
+#[error_code]
+pub enum Error {
+    #[msg("User name is too long")]
+    NameTooLong,
+}
+
 #[program]
 pub mod anchor_pda_user_stats {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn open(ctx: Context<Open>, name: String) -> Result<()> {
+        let user_stats = &mut ctx.accounts.user_stats;
+
+        if name.as_bytes().len() > UserStats::NAME_MAX {
+            Err(Error::NameTooLong)?;
+        }
+        user_stats.name = name;
+        user_stats.bump = *ctx.bumps.get("user_stats").unwrap();
+
+        Ok(())
+    }
+
+    pub fn close(ctx: Context<Close>) -> Result<()> {
+        let user_stats = &mut ctx.accounts.user_stats;
+        let lamports = **user_stats.to_account_info().lamports.borrow();
+
+        // Transfer the lamports back to the user to close the PDA account.
+        let user = &mut ctx.accounts.user;
+        **user.to_account_info().lamports.borrow_mut() += lamports;
+        **user_stats.to_account_info().lamports.borrow_mut() -= lamports;
         Ok(())
     }
 }
 
+#[account]
+pub struct UserStats {
+    /// A user name, 32 bytes max.
+    name: String,
+
+    /// A PDA bump.
+    bump: u8,
+}
+
+impl UserStats {
+    /// A space for the UserStats
+    const SPACE: usize = Self::DESCRIMINATOR + 4 + Self::NAME_MAX + 1;
+    const DESCRIMINATOR: usize = 8;
+    const NAME_MAX: usize = 32;
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Open<'info> {
+    /// A user, who pays for the `UserStats` account.
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    /// A `UserStats` PDA account.
+    #[account(
+        init,
+        payer = user,
+        space = UserStats::SPACE,
+        seeds = [b"user-stats", user.key().as_ref()],
+        bump
+    )]
+    pub user_stats: Account<'info, UserStats>,
+
+    /// SystemProgram to create PDA account.
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Close<'info> {
+    /// A user account to get the rent back.
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    /// A PDA account to be closed.
+    #[account(
+        mut,
+        seeds = [b"user-stats", user.key().as_ref()],
+        bump
+    )]
+    pub user_stats: Account<'info, UserStats>,
+
+    /// SystemProgram to close the PDA account.
+    pub system_program: Program<'info, System>,
+}
--- anchor-pda-user-stats/tests/anchor-pda-user-stats.ts
+++ anchor-pda-user-stats/tests/anchor-pda-user-stats.ts
@@ -1,17 +1,56 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import { AnchorError, Program, web3 } from "@project-serum/anchor";
 import { AnchorPdaUserStats } from "../target/types/anchor_pda_user_stats";
+import { expect } from 'chai';
 
 describe("anchor-pda-user-stats", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace
     .AnchorPdaUserStats as Program<AnchorPdaUserStats>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("open a user stats", async () => {
+    const [userStatsPda, _] = web3.PublicKey
+      .findProgramAddressSync(
+        [
+          anchor.utils.bytes.utf8.encode("user-stats"),
+          provider.wallet.publicKey.toBuffer()
+        ],
+        program.programId
+      );
+
+    await program.methods
+      .open("keith")
+      .accounts({
+        user: provider.wallet.publicKey,
+        userStats: userStatsPda,
+      })
+      .rpc();
+
+    expect((await program.account.userStats.fetch(userStatsPda)).name)
+      .to.equal("keith");
+
+    // close the stats.
+    let tx = await program.methods
+      .close()
+      .accounts({
+        user: provider.wallet.publicKey,
+        userStats: userStatsPda,
+      })
+      .rpc();
+
+    console.log("sig to close pda", tx);
+
+    // I hope I know a better to way to catch the
+    // exception.  to.throw doesn't work with Promise.
+    let resp;
+    try {
+      await program.account.userStats.fetch(userStatsPda);
+    } catch (e) {
+      resp = e;
+    }
+    expect(resp).to.be.instanceof(Error);
   });
 });
