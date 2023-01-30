1e30547c90da18a3f9d3a737a9b5b931ff3063dd
Keith Noguchi <keith@noguchi.us>
2023-01-29 20:37:56 -0800

    anchor-multisig3: Add open instruction

--- anchor-multisig3/programs/anchor-multisig3/src/lib.rs
+++ anchor-multisig3/programs/anchor-multisig3/src/lib.rs
@@ -1,15 +1,128 @@
+//! A native SOL multisig wallet program.
+
+use std::collections::HashSet;
+
 use anchor_lang::prelude::*;
 
 declare_id!("3LuouAGwBeueVADEviTaKLsgwkrinvfXKCNKPWcmbAQX");
+
+#[error_code]
+pub enum Error {
+    #[msg("Not enough signers given")]
+    NotEnoughSigners,
+
+    #[msg("Too many signers given")]
+    TooManySigners,
+
+    #[msg("Threshold too high")]
+    ThresholdTooHigh,
+}
+
+/// A native SOL multisig wallet.
+#[account]
+pub struct Multisig {
+    /// A PDA bump of the account.
+    bump: u8,
+
+    /// A threshold.
+    m: u8,
+
+    /// A number of signers.
+    n: u8,
+
+    /// An array of signers.
+    signers: [Pubkey; 5],
+
+    /// An array of queued transactions.
+    txs: Vec<Pubkey>,
+}
+
+impl Multisig {
+    /// A minimum signers.
+    const MIN_SIGNERS: usize = 2;
+
+    /// A maximum signers.
+    const MAX_SIGNERS: usize = 5;
+
+    /// A maximum transactions to be queued.
+    const MAX_TXS: usize = 10;
+
+    /// A account space.
+    const SPACE: usize = 8 + 1 + 1 + 1 + 32 * Self::MAX_SIGNERS + 4 + 32 * Self::MAX_TXS;
+}
+
+#[derive(Accounts)]
+#[instruction(bump: u8)]
+pub struct Open<'info> {
+    /// A funder of the multisig account.
+    #[account(mut)]
+    pub funder: Signer<'info>,
+
+    /// A multisig account.
+    #[account(
+        init,
+        payer = funder,
+        space = Multisig::SPACE,
+        seeds = [b"multisig", funder.key.as_ref()],
+        bump,
+    )]
+    pub multisig: Account<'info, Multisig>,
+
+    /// The system program to create a multisig PDA account.
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(bump: u8)]
+pub struct Close<'info> {
+    /// An original funder of the multisig account.
+    #[account(mut)]
+    pub funder: Signer<'info>,
+
+    /// A multisig account.
+    #[account(mut, close = funder, seeds = [b"multisig", funder.key.as_ref()], bump)]
+    pub multisig: Box<Account<'info, Multisig>>,
+
+    /// The system program to transfer back the fund.
+    pub system_program: Program<'info, System>,
+}
 
 #[program]
 pub mod anchor_multisig3 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn open(ctx: Context<Open>, bump: u8, m: u8, signers: Vec<Pubkey>) -> Result<()> {
+        let multisig = &mut ctx.accounts.multisig;
+        let funder = &ctx.accounts.funder;
+
+        // Checks duplicate signers.
+        let mut signers: HashSet<_> = signers.into_iter().collect();
+        signers.insert(funder.key());
+
+        // Makes sure we have a valid number of sighers,
+        // as well as the valid threshold, m <= signers.len().
+        require_gte!(
+            signers.len(),
+            Multisig::MIN_SIGNERS,
+            Error::NotEnoughSigners
+        );
+        require!(signers.len() < Multisig::MAX_SIGNERS, Error::TooManySigners);
+        let threshold = m as usize;
+        require_gte!(signers.len(), threshold, Error::ThresholdTooHigh);
+
+        // Initializes the multisig PDA account.
+        multisig.bump = bump;
+        multisig.m = m;
+        multisig.n = signers.len() as u8;
+        signers
+            .into_iter()
+            .enumerate()
+            .for_each(|(i, signer)| multisig.signers[i] = signer);
+
+        Ok(())
+    }
+
+    pub fn close(_ctx: Context<Close>, _bump: u8) -> Result<()> {
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
--- anchor-multisig3/tests/anchor-multisig3.ts
+++ anchor-multisig3/tests/anchor-multisig3.ts
@@ -1,16 +1,63 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import { web3, Program } from "@project-serum/anchor";
 import { AnchorMultisig3 } from "../target/types/anchor_multisig3";
+import { expect } from "chai";
 
 describe("anchor-multisig3", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.AnchorMultisig3 as Program<AnchorMultisig3>;
+  const wallet = provider.wallet;
+  const [multisig, bump] = web3.PublicKey.findProgramAddressSync(
+    [anchor.utils.bytes.utf8.encode("multisig"), wallet.publicKey.toBuffer()],
+    program.programId
+  );
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const threshold = 2;
+  const signers = [];
+  signers.push(wallet.payer);
+  signers.push(web3.Keypair.generate());
+  signers.push(web3.Keypair.generate());
+  const payees = [];
+  payees.push(web3.Keypair.generate());
+  payees.push(web3.Keypair.generate());
+  payees.push(web3.Keypair.generate());
+
+  beforeEach(async () => {
+    await program.methods
+      .open(
+        bump,
+        threshold,
+        signers.map((pair) => pair.publicKey)
+      )
+      .accounts({ funder: wallet.publicKey, multisig })
+      .signers([wallet.payer])
+      .rpc();
+  });
+
+  afterEach(async () => {
+    try {
+      await program.methods
+        .close(bump)
+        .accounts({ founder: wallet.publicKey, multisig })
+        .signers([wallet.payer])
+        .rpc();
+    } catch (_) {
+      // ignore the closing errors.
+    }
+  });
+
+  it("Checks the multisig initial state", async () => {
+    const ms = await program.account.multisig.fetch(multisig);
+    expect(ms.bump).to.equal(bump);
+    expect(ms.m).to.equal(threshold);
+    expect(ms.n).to.equal(signers.length);
+    expect(ms.signers).to.include.deep.members(
+      signers.map((pair) => pair.publicKey)
+    );
+    expect(ms.signers).to.have.lengthOf(5);
+    expect(ms.txs).to.have.lengthOf(0);
   });
 });
